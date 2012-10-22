34463107a0bdb04d0984b34cb89364fd6da5ce70
Dan Draper <daniel@codefire.com>
2012-10-22 17:47:30 +1030

    Changed generator template to use older syntax for backwards compat

--- lib/generators/settingcrazy/setting_values_migration/templates/migration.rb
+++ lib/generators/settingcrazy/setting_values_migration/templates/migration.rb
@@ -1,10 +1,14 @@
 class CreateSettingsValues < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :setting_values do |t|
       t.string :value
       t.string :key
       t.references :settable, :polymorphic => true
       t.timestamps
     end
+  end
+
+  def self.down
+    drop_table :setting_values
   end
 end
